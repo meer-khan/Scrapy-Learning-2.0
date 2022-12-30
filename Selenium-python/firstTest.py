03fc441bbc70d30654551518e24e6599d63882d5
Shahmeer <shahmirkhan519@gmail.com>
2022-12-30 11:24:57 +0500

    stabilizing the chrome

--- Selenium-python/firstTest.py
+++ Selenium-python/firstTest.py
@@ -2,12 +2,28 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element(By.NAME, "q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+# driver = webdriver.Chrome()
+# driver.get("https://www.python.org/")
+# assert "Python" in driver.title
+# elem = driver.find_element(By.NAME, "q")
+# elem.clear()
+# elem.send_keys("pycon")
+# elem.send_keys(Keys.RETURN)
+# assert "No results found." not in driver.page_source
+# driver.close()
+
+def launchBrowser():
+    # chrome_options = Options()
+    driver = webdriver.Chrome(executable_path="chromedriver.exe")
+    driver.get("https://www.python.org/")
+
+
+
+# def launchBrowser():
+#    chrome_options = Options()
+#    chrome_options.binary_location="../Google Chrome"
+#    chrome_options.add_argument("disable-infobars");
+#    driver = webdriver.Chrome(chrome_options=chrome_options)
+
+#    driver.get("http://www.google.com/")
+launchBrowser()
